ea86d3bec5588d4ca812e5d12ddc3ef5fb834ebb
Aladiev <aladjev.ilia2010@yandex.ru>
2024-03-17 14:16:55 +0300

    key prop

--- src/components/board/generateFigures.tsx
+++ src/components/board/generateFigures.tsx
@@ -12,33 +12,33 @@ export default function generateFigures(board: boardType, clickedPosition: strin
     const figures = [];
 
     for (const letter of letters) {
-        figures.push(<Pawn board={board} letter={letter} color='black' onClick={cellOnClick} clickedPosition={clickedPosition} uid={letter} />);
-        figures.push(<Pawn board={board} letter={letter} color='white' onClick={cellOnClick} clickedPosition={clickedPosition} uid={letter} />);
+        figures.push(<Pawn board={board} letter={letter} color='black' onClick={cellOnClick} clickedPosition={clickedPosition} uid={letter} key={'pawnblack' + letter}/>);
+        figures.push(<Pawn board={board} letter={letter} color='white' onClick={cellOnClick} clickedPosition={clickedPosition} uid={letter} key={'pawnwhite' + letter}/>);
     }
 
-    figures.push(<King board={board} color='black' onClick={cellOnClick} clickedPosition={clickedPosition} uid='king-black' />);
-    figures.push(<King board={board} color='white' onClick={cellOnClick} clickedPosition={clickedPosition} uid='king-white' />);
+    figures.push(<King board={board} color='black' onClick={cellOnClick} clickedPosition={clickedPosition} uid='king-black' key={'king-black'}/>);
+    figures.push(<King board={board} color='white' onClick={cellOnClick} clickedPosition={clickedPosition} uid='king-white' key={'king-white'}/>);
 
-    figures.push(<Queen board={board} color='black' onClick={cellOnClick} clickedPosition={clickedPosition} uid='queen-black' />);
-    figures.push(<Queen board={board} color='white' onClick={cellOnClick} clickedPosition={clickedPosition} uid='queen-white' />);
+    figures.push(<Queen board={board} color='black' onClick={cellOnClick} clickedPosition={clickedPosition} uid='queen-black' key={'queen-black'}/>);
+    figures.push(<Queen board={board} color='white' onClick={cellOnClick} clickedPosition={clickedPosition} uid='queen-white' key={'queen-white'}/>);
 
-    figures.push(<Rook letter='A' board={board} color='black' onClick={cellOnClick} clickedPosition={clickedPosition} uid='rook-A-black' />);
-    figures.push(<Rook letter='A' board={board} color='white' onClick={cellOnClick} clickedPosition={clickedPosition} uid='rook-A-white' />);
+    figures.push(<Rook letter='A' board={board} color='black' onClick={cellOnClick} clickedPosition={clickedPosition} uid='rook-A-black' key={'rook-A-black'}/>);
+    figures.push(<Rook letter='A' board={board} color='white' onClick={cellOnClick} clickedPosition={clickedPosition} uid='rook-A-white' key={'rook-A-white'}/>);
 
-    figures.push(<Rook letter='H' board={board} color='black' onClick={cellOnClick} clickedPosition={clickedPosition} uid='rook-H-black' />);
-    figures.push(<Rook letter='H' board={board} color='white' onClick={cellOnClick} clickedPosition={clickedPosition} uid='rook-H-white' />);
+    figures.push(<Rook letter='H' board={board} color='black' onClick={cellOnClick} clickedPosition={clickedPosition} uid='rook-H-black' key={'rook-H-black'}/>);
+    figures.push(<Rook letter='H' board={board} color='white' onClick={cellOnClick} clickedPosition={clickedPosition} uid='rook-H-white' key={'rook-H-white'}/>);
 
-    figures.push(<Bishop letter='C' board={board} color='black' onClick={cellOnClick} clickedPosition={clickedPosition} uid='bishop-C-black' />);
-    figures.push(<Bishop letter='C' board={board} color='white' onClick={cellOnClick} clickedPosition={clickedPosition} uid='bishop-C-white' />);
+    figures.push(<Bishop letter='C' board={board} color='black' onClick={cellOnClick} clickedPosition={clickedPosition} uid='bishop-C-black' key={'bishop-C-black'}/>);
+    figures.push(<Bishop letter='C' board={board} color='white' onClick={cellOnClick} clickedPosition={clickedPosition} uid='bishop-C-white' key={'bishop-C-white'}/>);
 
-    figures.push(<Bishop letter='F' board={board} color='black' onClick={cellOnClick} clickedPosition={clickedPosition} uid='bishop-F-black' />);
-    figures.push(<Bishop letter='F' board={board} color='white' onClick={cellOnClick} clickedPosition={clickedPosition} uid='bishop-F-white' />);
+    figures.push(<Bishop letter='F' board={board} color='black' onClick={cellOnClick} clickedPosition={clickedPosition} uid='bishop-F-black' key={'bishop-F-black'}/>);
+    figures.push(<Bishop letter='F' board={board} color='white' onClick={cellOnClick} clickedPosition={clickedPosition} uid='bishop-F-white' key={'bishop-F-white'}/>);
 
-    figures.push(<Knight letter='B' board={board} color='black' onClick={cellOnClick} clickedPosition={clickedPosition} uid='knight-B-black' />);
-    figures.push(<Knight letter='B' board={board} color='white' onClick={cellOnClick} clickedPosition={clickedPosition} uid='knight-B-white' />);
+    figures.push(<Knight letter='B' board={board} color='black' onClick={cellOnClick} clickedPosition={clickedPosition} uid='knight-B-black' key={'knight-B-black'}/>);
+    figures.push(<Knight letter='B' board={board} color='white' onClick={cellOnClick} clickedPosition={clickedPosition} uid='knight-B-white' key={'knight-B-white'}/>);
 
-    figures.push(<Knight letter='G' board={board} color='black' onClick={cellOnClick} clickedPosition={clickedPosition} uid='knight-G-black' />);
-    figures.push(<Knight letter='G' board={board} color='white' onClick={cellOnClick} clickedPosition={clickedPosition} uid='knight-G-white' />);
+    figures.push(<Knight letter='G' board={board} color='black' onClick={cellOnClick} clickedPosition={clickedPosition} uid='knight-G-black' key={'knight-G-black'}/>);
+    figures.push(<Knight letter='G' board={board} color='white' onClick={cellOnClick} clickedPosition={clickedPosition} uid='knight-G-white' key={'knight-G-white'}/>);
 
     return figures;
 }
